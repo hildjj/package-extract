b177c03a7ceecdd1d4bfc64fe78a009467622850
Joe Hildebrand <joe-github@cursive.net>
2025-07-04 13:52:32 -0600

    Fix lint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,6 @@ import markdown from '@cto.af/eslint-config/markdown.js';
 import mod from '@cto.af/eslint-config/module.js';
 
 export default [
-  {
-    ignores: [
-      '**/*.d.ts',
-    ],
-  },
   ...base,
   ...mod,
   ...markdown,
